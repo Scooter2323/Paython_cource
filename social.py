498cbc1170296f203b2de12fba5d6ea56e862323
Scooter2323 <54883054+Scooter2323@users.noreply.github.com>
2020-12-14 17:51:19 +0200

    Add files via upload

---
+++ social.py
@@ -0,0 +1,19 @@
+#AKA -database
+#USER DATA - hardcoded
+user_name   = "johny888"                 # str
+user_email  = "johny@somemail.examle"    # str
+user_online = False                      # bool
+user_last   = 15                         # int
+user_rank   = 4.5                        # float
+
+
+# LOGIC / PROCESSING
+user_last  = user_last +5
+
+# PRESENTATION / OUTPUT
+print( "######## USER DATA ########" )
+print( "name:   " , user_name )
+print( "email:  " , user_email )
+print( "online: " , user_email ) #CTRL +SHIFT + D -duplicate
+print( "last:   " , user_last ,"mins" ) #CTRL +clic -multicursor
+print( "rank:   " , user_rank ,"start" )
