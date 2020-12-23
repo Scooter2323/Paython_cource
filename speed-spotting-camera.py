c6f395f1525c36deb7f1bc141812bf060432ccf7
Scooter2323 <54883054+Scooter2323@users.noreply.github.com>
2020-12-23 22:40:10 +0200

    Add files via upload

---
+++ speed-spotting-camera.py
@@ -0,0 +1,12 @@
+car_speed   = 50             # km/h
+SPEED_LIMIT = 70             # km/h
+
+if car_speed > SPEED_LIMIT: 
+    print( "The driver will be punished with a fee of 50 U.C." )
+
+print ( "Job done !!! ")   
+ #################################################################
+
+#[input data] -->  <car_speed > SPEED_LIMIT???>  --> True --> "Punished" --> "Done!"
+#                                               \-> False----------------------^ 
+     
