ff63fb0979cac4d53a309dc6e586ee6bc88109a3
Scooter2323 <54883054+Scooter2323@users.noreply.github.com>
2021-01-27 05:28:41 +0200

    Add files via upload

---
+++ medium.py
@@ -0,0 +1,14 @@
+# define the function
+def bye ( lang ):                # parameter  - lang
+    if lang == "en":
+        print("Good buy!!!")
+    if lang == "ro":
+            print("La revedere!!!")
+    if lang == "ru":
+            print("До свидание!!!")
+# call the function
+# HW1: upgrade the logic of  "bye ()" -> "bye( lang )"
+#      * if lang -   
+bye( "en" )
+bye( "ro" )
+bye( "ru" )
